5d65ba43ef3899a65b047f5753f9893cbf9e8b08
Marc Tsobgny <marctsob2321@gmail.com>
2022-05-12 22:22:50 +0100

    Initialisation de l'API GoogleMap

--- IndoorPlaningApp/IndoorApp/static/scripts/map.js
+++ IndoorPlaningApp/IndoorApp/static/scripts/map.js
@@ -1,4 +1,3 @@
-alert("hello map");
 function myMap() {
     var mapProp= {
       center:new google.maps.LatLng(51.508742,-0.120850),
@@ -6,3 +5,6 @@ function myMap() {
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     }
+
+
+// var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
